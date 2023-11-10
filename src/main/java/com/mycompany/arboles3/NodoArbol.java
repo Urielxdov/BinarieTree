4af49f4a0b2d004fe4340d7be6015b3f90336756
Urielxdov <Urieledgar878@gmail.com>
2023-11-09 21:32:49 -0600

    Correcion total por que me regresaron el trabajo por que olvide usar el codigo de clase, pipipi

--- src/main/java/com/mycompany/arboles3/NodoArbol.java
+++ src/main/java/com/mycompany/arboles3/NodoArbol.java
@@ -1,58 +1,17 @@
 package com.mycompany.arboles3;
 
-/**
- * En este metodo damos la platilla para la creacion de nodos en los arboles
- * teniendo la idea de que se un arbol binario, debera trabajar con indices
- * para ser capaz de realizar operaciones como busqueda, eliminacion, entre otros
- * @author Zomber
- */
-public class NodoArbol <T> {
-    private NodoArbol derecha, izquierda;
-    private T dato;
-    
-    
+public class NodoArbol <T extends Integer>{
+    public T dato;
+    public NodoArbol<T> izq;
+    public NodoArbol<T> der;
+    NodoArbol<T> padre;        
+
     public NodoArbol(T dato) {
-        this.derecha = null;
-        this.izquierda = null;
         this.dato = dato;
-    }
+    }               
 
-    public NodoArbol(NodoArbol<T> derecha, NodoArbol<T> izquierda, T dato) {
-        this.derecha = derecha;
-        this.izquierda = izquierda;
-        this.dato = dato;
+    @Override
+    public String toString() {
+        return " " + dato + " ";
     }
-
-    public NodoArbol(T dato, NodoArbol<T> derecha) {
-        this(derecha, null, dato);
-    }
-
-    public NodoArbol(NodoArbol<T> izquierda, T dato) {
-        this(null, izquierda, dato);
-    }
-    public NodoArbol getDerecha() {
-        return derecha;
-    }
-
-    public void setDerecha(NodoArbol derecha) {
-        this.derecha = derecha;
-    }
-
-    public NodoArbol getIzquierda() {
-        return izquierda;
-    }
-
-    public void setIzquierda(NodoArbol izquierda) {
-        this.izquierda = izquierda;
-    }
-
-    public T getDato() {
-        return dato;
-    }
-
-    public void setDato(T dato) {
-        this.dato = dato;
-    }  
-    
-    
 }
